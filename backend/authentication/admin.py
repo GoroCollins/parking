e56b72f82afc8faff8b43354d316d285bee0c1f1
Goro Collins <gorocollins@gmail.com>
2026-04-07 14:28:01 +0300

    add apps to admin panel

--- backend/authentication/admin.py
+++ backend/authentication/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django import forms
 from authentication.models import CustomUser
-
 # Register your models here.
 
 
