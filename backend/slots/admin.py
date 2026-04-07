5cc8a2242dd27347bf053ea589246d6ff93b4b8a
Goro Collins <gorocollins@gmail.com>
2026-04-07 14:28:18 +0300

    add apps to admin panel

--- backend/slots/admin.py
+++ backend/slots/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from slots.models import ParkingArea, ParkingSession, ParkingSlot, Payment
 
 # Register your models here.
+admin.site.register(ParkingArea)
+admin.site.register(ParkingSlot)
+admin.site.register(ParkingSession) # make read only
+admin.site.register(Payment) # make read only
