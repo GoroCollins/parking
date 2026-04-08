93e7f4da1e121988a1ea97d41ed2732f383c950c
Goro Collins <gorocollins@gmail.com>
2026-04-08 15:44:20 +0300

    make payments and sessions readonly

--- backend/slots/admin.py
+++ backend/slots/admin.py
@@ -4,5 +4,31 @@ from slots.models import ParkingArea, ParkingSession, ParkingSlot, Payment
 # Register your models here.
 admin.site.register(ParkingArea)
 admin.site.register(ParkingSlot)
-admin.site.register(ParkingSession) # make read only
-admin.site.register(Payment) # make read only
+
+@admin.register(ParkingSession)
+class ParkingSessionAdmin(admin.ModelAdmin):
+    list_display = ("id", "slot", "amount", "duration", )
+    readonly_fields = [field.name for field in ParkingSession._meta.fields]
+
+    def has_add_permission(self, request):
+        return True
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+    
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ("id", "payment_type", "amount", "receipted_by", "transaction_date", )
+    readonly_fields = [field.name for field in Payment._meta.fields]
+
+    def has_add_permission(self, request):
+        return True
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
